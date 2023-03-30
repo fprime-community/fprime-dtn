9332a3fd75ded90bf7de6bd132370a68c6dc3433
Paul Rosemurgy <paul.c.rosemurgy@jpl.nasa.gov>
2023-03-29 17:48:41 -0700

    Reformat `bpchat.c`

--- Dtn/Dtn/Framer/bpchat.h
+++ Dtn/Dtn/Framer/bpchat.h
@@ -13,6 +13,7 @@
 //! @param _ownEid own EID, intentionally mutable to appease ION
 //! @param _destEid destination EID, intentionally mutable to appease ION
 int bpchat_start(char *_ownEid, char *_destEid);
+
 int bpchat_send(const char *buffer, size_t size);
 
 #endif // BPCHAT_H
